2dce39df6db8a590a74f0231c094fd5f197f8f02
ksenia topchu <pureshka7@gmail.com>
2025-12-19 15:56:27 +0200

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<locale.h>
+#include<stdlib.h>
+#include<time.h>
+void main(void)
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int n=5;
+	int* ptr = (int*)calloc(n, sizeof(int));
+	if (ptr == NULL)
+	{
+		printf("ошибка выделения памяти");
+		return 1;
+	}
+	for (int i = 0;i < n;i++)
+		printf("%5i", ptr[i]);
+	free(ptr);
+	ptr = NULL;
+	return 0;
+}
